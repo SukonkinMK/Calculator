47c73b027194fbef437df035488fc3ef18ab4e72
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-11-27 13:26:36 +0300

    Консольный калькулятор математических выражений

---
+++ Calculator/ConsoleUI.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator
+{
+    internal class ConsoleUI : View
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public string ReadLine()
+        {
+            string line = Console.ReadLine()!;
+            return line;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Calculator;
+
+Presenter presenter = new Presenter(new ConsoleUI(), new MathModel());
+presenter.Run();
